7fdb3055fab39b5be95bea9204496044e63665c5
Mohammed Badat <moebadat16@gmail.com>
2023-04-24 10:49:29 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { HomeComponent } from './components/home/home.component';
 import { AddComponent } from './components/add/add.component';
 import { UpdateComponent } from './components/update/update.component';
 import { ViewComponent } from './components/view/view.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
-  {path : '/', component : HomeComponent},
-  {path : '/add', component : AddComponent},
-  {path : '/update', component : UpdateComponent},
-  {path : '/view', component : ViewComponent}];
+  {path : '', component : HomeComponent},
+  {path : 'add', component : AddComponent},
+  {path : 'update', component : UpdateComponent},
+  {path : 'view', component : ViewComponent}];
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),BrowserModule, HttpClientModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
