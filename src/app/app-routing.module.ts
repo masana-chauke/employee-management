776e883223a28d22d41eab1725f97ce8e1b5c617
Mohammed Badat <moebadat16@gmail.com>
2023-04-24 10:38:08 +0200

    browser module err

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AddComponent } from './components/add/add.component';
+import { UpdateComponent } from './components/update/update.component';
+import { ViewComponent } from './components/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '/', component : HomeComponent},
+  {path : '/add', component : AddComponent},
+  {path : '/update', component : UpdateComponent},
+  {path : '/view', component : ViewComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
