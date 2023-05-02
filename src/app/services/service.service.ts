d3f984f7e5df295ee1b452e9fa192dc4c2e5c172
Mohammed Badat <moebadat16@gmail.com>
2023-05-02 11:27:12 +0200

    added url for api

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -7,48 +7,45 @@ import { Injectable } from '@angular/core';
 export class ServiceService {
   constructor(private http:HttpClient) { }
 
+  private url ='http://localhost:8080/employees';
+
   /* public getData(id:any):Observable< any[]>{
-    return this.http.get<any>("https://fakestoreapi.com/products")
+    return this.http.get<any>("this.url")
   }
 
   public getOne(id:any):Observable <any[]>{
-    return this.http.get<any>(`https://fakestoreapi.com/products${id}`)
+    return this.http.get<any>(`http://localhost:8080/employees${id}`)
   }
 
   public addData(val:any):Observable<any>{
-    return this.http.post("https://fakestoreapi.com/products", val)
+    return this.http.post(this.url, val)
   }
 
   public updateData(val:any, id:any){
-    return this.http.put("https://fakestoreapi.com/products"+id, val)
+    return this.http.put(this.url+id, val)
   }
   public deleteData(id:any){
-    return this.http.delete("https://fakestoreapi.com/products"+id)
-
-
+    return this.http.delete(this.url+id)
   } */
 
 
   getEmployeeAll(){
-    return this.http.get('https://api.slingacademy.com/v1/sample-data/users')
-
+    return this.http.get(this.url)
   }
-
+  
   getEmployee(id:number){
-    return this.http.get('https://fakestoreapi.com/products%27'+id)
-
+    return this.http.get(this.url+id)
   }
-
 
   addEmployee(body:any){
-    this.http.post('https://fakestoreapi.com/products%27',body)
+    this.http.post(this.url,body)
   }
 
   updateEmployee(id:number,body:any){
-      return this.http.put('https://fakestoreapi.com/products%27'+id,body)
+      return this.http.put(this.url+id,body)
   }
 
  deleteData(id:any){
-    return this.http.delete("https://fakestoreapi.com/products"+id)
+    return this.http.delete(this.url+id)
   }
 }
